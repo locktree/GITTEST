591d32836da528300d5cb7b308a6aa39cb5b3a67
locktree <mitchellslotboom@hotmail.com>
2019-05-30 20:35:18 +0200

    add new test

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+A simple python script template.
+@Author      : mitchell
+@Last Update : Thu 30 May 2019 08:34:08 PM CEST
+"""
+import os
+import sys
+import argparse
+
+def main():
+    print ("Hello world")
+
+if __name__ == '__main__':
+    main()
+###########################################################################
+###                          E O F
+###########################################################################
