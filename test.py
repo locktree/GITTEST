ea5ed9a38b1e85e08289d47c7e491c42fc3f3936
locktree <mitchellslotboom@hotmail.com>
2019-12-25 13:13:13 +0100

    some comment

--- test.py
+++ test.py
@@ -3,13 +3,14 @@
 """
 A simple python script template.
 @Author      : mitchell
-@Last Update : Wed 25 Dec 2019 01:10:29 PM CET
+@Last Update : Wed 25 Dec 2019 01:12:59 PM CET
 """
 import os
 import sys
 import argparse
 
 def main():
+    ## some comment
     ## some comment
     print ("Hello world")
     print ("Hello world")
