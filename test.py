26e540f28e07dd3567af6422ee623f562ce80ed0
locktree <mitchellslotboom@hotmail.com>
2020-07-08 09:39:13 +0200

    Add from repo1

--- test.py
+++ test.py
@@ -4,9 +4,9 @@
 A simple python script template.
 @Author      : mitchell
 <<<<<<< Updated upstream
-@Last Update : Wed 08 Jul 2020 09:35:37 AM CEST
+@Last Update : Wed 08 Jul 2020 09:39:03 AM CEST
 ||||||| merged common ancestors
-@Last Update : Wed 08 Jul 2020 09:35:37 AM CEST
+@Last Update : Wed 08 Jul 2020 09:39:03 AM CEST
 =======
 @Last Update : Wed 08 Jul 2020 09:01:41 AM CEST
 >>>>>>> Stashed changes
@@ -22,6 +22,7 @@ def main():
     print ("Hello world")
     print ("Hello world3")
     print ("Hello world")
+    this is some line from repo1 - master
 
 if __name__ == '__main__':
     main()
