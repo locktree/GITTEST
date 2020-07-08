e02022207829e8dca0b3ab786f4a4e965460f1fb
locktree <mitchellslotboom@hotmail.com>
2020-07-08 10:23:12 +0200

    solve merge conflict

--- test.py
+++ test.py
@@ -3,13 +3,7 @@
 """
 A simple python script template.
 @Author      : mitchell
-<<<<<<< Updated upstream
-@Last Update : Wed 08 Jul 2020 10:18:21 AM CEST
-||||||| merged common ancestors
-@Last Update : Wed 08 Jul 2020 10:18:21 AM CEST
-=======
-@Last Update : Wed 08 Jul 2020 09:01:41 AM CEST
->>>>>>> Stashed changes
+@Last Update : Wed 08 Jul 2020 10:22:59 AM CEST
 """
 import os
 import sys
